2317f808aba4134a7082eefae0289d2aa3226494
Angel Aguirre <angelaguirrel@hotmail.com>
2021-12-19 18:40:11 -0600

    Creación de ideas a traves del formulario

---
+++ app/Http/Livewire/CreateIdea.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use App\Models\Idea;
+use Illuminate\Http\Response;
+use Livewire\Component;
+
+class CreateIdea extends Component
+{
+    public $title;
+    public $category = 1;
+    public $description;
+
+    private $idea_status_on_create = 1; //Open status
+
+    protected $rules = [
+        'title'         => 'required|min:4',
+        'category'      => 'required|integer',
+        'description'   => 'required|min:4',
+    ];
+
+    public function render()
+    {
+        return view('livewire.create-idea', [
+            'categories' => Category::all(),
+        ]);
+    }
+    
+    /**
+     * Create a new idea and reload the index page
+     */
+    public function createIdea() {
+        if(auth()->check()) {
+            $this->validate();
+
+            Idea::create([
+                'user_id' => auth()->id(),
+                'category_id' => $this->category,
+                'status_id' => $this->idea_status_on_create,
+                'title' => $this->title,
+                'description' => $this->description,
+            ]);
+
+            session()->flash('success_message', 'Idea created successfully!');
+
+            $this->reset();
+
+            return redirect()->route('idea.index');
+        }
+
+        abort(Response::HTTP_FORBIDDEN);
+    }
+}
