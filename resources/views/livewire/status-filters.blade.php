9e91d73709f48f47000f91274e3edec7eefe904b
Angel Aguirre <angelaguirrel@hotmail.com>
2022-01-19 21:12:13 -0600

    Tests para el contador del filtro de status

--- resources/views/livewire/status-filters.blade.php
+++ resources/views/livewire/status-filters.blade.php
@@ -2,7 +2,7 @@
     <nav class=" hidden md:flex items-center justify-between text-sm text-gray-400">
         <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
             <li>
-                <a wire:click.prevent="setStatus('All')" href="#" class="border-b-4 pb-3 hover:border-blue @if ($status == 'All') border-blue text-gray-900 @endif">All ideas({{ $statusCount['all_statuses'] }})</a>
+                <a wire:click.prevent="setStatus('All')" href="#" class="border-b-4 pb-3 hover:border-blue @if ($status == 'All') border-blue text-gray-900 @endif">All Ideas ({{ $statusCount['all_statuses'] }})</a>
             </li>
             <li>
                 <a wire:click.prevent="setStatus('Considering')" href="#" class="transition duration-100 ease-in border-b-4 pb-3 hover:border-blue @if ($status == 'Considering') border-blue text-gray-900 @endif">
