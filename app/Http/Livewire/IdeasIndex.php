fe0d5e41a13b638c3d2e0cde41e74354544e62b5
Angel Aguirre <angelaguirrel@hotmail.com>
2022-01-23 19:13:55 -0600

    Filtros por categoria

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Vote;
 use Livewire\Component;
@@ -11,13 +12,32 @@ class IdeasIndex extends Component
 {
     use WithPagination;
 
+    public $status = 'All';
+    public $category;
+
+
+    protected $queryString = [
+        'status',
+        'category',
+    ];
+
+    protected $listeners = ['queryStringUpdatedStatus'];
+
+    public function mount() {
+        $this->status = request()->status ?? 'All';
+    }
+
     public function render()
     {
         return view('livewire.ideas-index', [
             'ideas' => Idea::with('user', 'category', 'status')
                 ->join('statuses', 'statuses.id', '=', 'ideas.status_id')
-                ->when(request()->status && request()->status != 'All', function($query) {
-                    $query->where('statuses.name', request()->status);
+                ->when($this->status && $this->status != 'All', function($query) {
+                    $query->where('statuses.name', $this->status);
+                })
+                ->join('categories', 'categories.id', '=', 'ideas.category_id')
+                ->when($this->category && $this->category != 'All Categories', function($query) {
+                    $query->where('categories.name', $this->category);
                 })
                 ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', auth()->id())
@@ -26,6 +46,22 @@ class IdeasIndex extends Component
                 ->withCount('votes')
                 ->orderBy('id', 'desc')
                 ->simplePaginate(Idea::PAGINATION_COUNT),
+            'categories' => Category::all(),
         ]);
+    }
+
+    /**
+     * Custom Methods
+     */
+    public function udatingCategory() {
+        $this->resetPage();
+    }
+
+    /**
+     * Event Listeners
+     */
+    public function queryStringUpdatedStatus($newStatus) {
+        $this->status = $newStatus;
+        $this->resetPage();
     }
 }
--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -8,18 +8,15 @@ use Livewire\Component;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
-
-    protected $queryString = [
-        'status'
-    ];
+    public $status;
+    public $statusCount;
 
     public function mount() {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if(Route::currentRouteName() == 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
@@ -33,12 +30,13 @@ class StatusFilters extends Component
      */
     public function setStatus($status) {
         $this->status = $status;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-        // if ($this->getPreviousRouteName() === 'idea.show') {{
+        if ($this->getPreviousRouteName() === 'idea.show') {
             return redirect()->route('idea.index', 
                 ['status' => $this->status]
             );
-        //}
+        }
     }
 
     public function getPreviousRouteName()
