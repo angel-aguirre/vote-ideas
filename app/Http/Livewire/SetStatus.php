026352a24e07c860df90a8b993154de0e7920fea
Angel Aguirre <angelaguirrel@hotmail.com>
2022-02-10 21:45:34 -0600

    Fucionalidad para notificar a los votantes cuando la idea cambia de status

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -3,13 +3,16 @@
 namespace App\Http\Livewire;
 
 use App\Models\Idea;
+use App\Mail\IdeaStatusUpdatedMailable;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class SetStatus extends Component
 {
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea) {
         $this->idea = $idea;
@@ -32,6 +35,21 @@ class SetStatus extends Component
         $this->idea->status_id = $this->status;
         $this->idea->save();
 
+        if ( $this->notifyAllVoters ) {
+            $this->notifyAllVoters();
+        }
+
         $this->emit('statusWasUpdated');
+    }
+
+    public function notifyAllVoters() {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) {
+                foreach ($voters as $voter) {
+                    Mail::to($voter)
+                        ->queue( new IdeaStatusUpdatedMailable( $this->idea ) );
+                }
+           });
     }
 }
