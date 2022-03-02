1045d7e1acc2e3e52f0d4937ce3fe70d3022ee4f
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-01 21:22:35 -0600

    Mostrando el total de notificaciones sin leer

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,13 +6,18 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFICATION_THRESHOLD = 10;
 
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
     
     protected $listeners = ['getNotifications'];
 
     public function mount() {
         $this->notifications = collect([]);
+        $this->isLoading = true;
+        $this->getNotificationCount();
     }
 
     public function render()
@@ -24,6 +29,24 @@ class CommentNotifications extends Component
      * Listeners
      */
     public function getNotifications() {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()
+            ->user()
+            ->unreadNotifications()
+            ->latest()
+            ->take(self::NOTIFICATION_THRESHOLD)
+            ->get();
+        
+        $this->isLoading = false;
+    }
+
+    /**
+     * Custom methods
+     */
+    public function getNotificationCount() {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if ($this->notificationCount > self::NOTIFICATION_THRESHOLD) {
+            $this->notificationCount = '10+';
+        }
     }
 }
